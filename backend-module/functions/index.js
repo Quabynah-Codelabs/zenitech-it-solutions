2d124714d8da0e18294286f0e917af7fe861ca5f
Dennis Kwabena Bilson Jr <41457025+Quabynah-Codelabs@users.noreply.github.com>
2019-05-26 14:59:59 +0000

    Before upgrade

--- backend-module/functions/index.js
+++ backend-module/functions/index.js
@@ -43,115 +43,112 @@ MongoClient.connect(url, {
     }
 });
 
-exports.testAPI = functions.https.onRequest((req,res) => {
-    return res.send({
-        message: 'Connected successfully to Firebase Cloud functions'
-    })
-});
+
+
 
 // Login Function
-exports.login = functions.https.onRequest(async (req, res) => {
-    var body = req.body;
-    // { "email" : "quaynah@gmail.com", "password" : "quabynah" }
+// exports.login = functions.https.onRequest(async (req, res) => {
+//     var body = req.body;
+//     // { "email" : "quaynah@gmail.com", "password" : "quabynah" }
 
-    if (body && req.method == 'POST') {
-        var email = body.email;
-        // var password = body.password;
+//     if (body && req.method == 'POST') {
+//         var email = body.email;
+//         // var password = body.password;
 
-        // Get auth key
-        var authKey = await jwt.sign({
-            foo: 'bar'
-        }, 'shhhhh');
+//         // Get auth key
+//         var authKey = await jwt.sign({
+//             foo: 'bar'
+//         }, 'shhhhh');
 
-        return res.status(201).send({
-            key: authKey,
-            name: '',
-            email: email,
-            avatar: '',
-            type: 'guest',
-            createdAt: new Date().getTime()
-        });
+//         return res.status(201).send({
+//             key: authKey,
+//             name: '',
+//             email: email,
+//             avatar: '',
+//             type: 'guest',
+//             createdAt: new Date().getTime()
+//         });
 
-    } else {
-        return res.status(400).send({
-            message: 'Bad login credentials'
-        });
-    }
+//     } else {
+//         return res.status(400).send({
+//             message: 'Bad login credentials'
+//         });
+//     }
 
-});
+// });
 
-// Login Function
-exports.register = functions.https.onRequest(async (req, res) => {
-    var body = req.body;
-    // { "email" : "quaynah@gmail.com", "password" : "quabynah" }
+// // Login Function
+// exports.register = functions.https.onRequest(async (req, res) => {
+//     var body = req.body;
+//     // { "email" : "quaynah@gmail.com", "password" : "quabynah" }
 
-    if (body && req.method == 'POST') {
-        var email = body.email;
-        // var password = body.password;
+//     if (body && req.method == 'POST') {
+//         var email = body.email;
+//         // var password = body.password;
 
-        // Get auth key
-        var authKey = await jwt.sign({
-            foo: 'bar'
-        }, 'shhhhh');
+//         // Get auth key
+//         var authKey = await jwt.sign({
+//             foo: 'bar'
+//         }, 'shhhhh');
 
-        return res.status(201).send({
-            key: authKey,
-            name: '',
-            email: email,
-            avatar: '',
-            type: 'guest',
-            createdAt: new Date().getTime()
-        });
+//         return res.status(201).send({
+//             key: authKey,
+//             name: '',
+//             email: email,
+//             avatar: '',
+//             type: 'guest',
+//             createdAt: new Date().getTime()
+//         });
 
-    } else {
-        return res.status(400).send({
-            message: 'Bad login credentials'
-        });
-    }
+//     } else {
+//         return res.status(400).send({
+//             message: 'Bad login credentials'
+//         });
+//     }
 
-});
+// });
 
-exports.products = functions.https.onRequest(async (req, res) => {
-    var path = req.path;
-    if (path !== undefined && path == '/') {
-        const products = await addFakeProducts();
-        return res.status(200).send(products);
-    } else {
-        var index = path.substr(1);
-        return res.status(200).send({
-            key: `${index}`,
-            name: `Item ${index}`,
-            price: gen(),
-            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            image: `${index % 2 == 0 ? "http://static.techspot.com/images/products/2019/laptops/org/2019-03-22-product-6.jpg": "https://www.androidpolice.com/wp-content/uploads/2018/12/Samsung-Galaxy-S10-Plus-5K_2.png"}`,
-            url: `https://us-central1-zenitech-solutions.cloudfunctions.net/products/${index}`,
-            uploadTime: new Date().getTime(),
-            quantity: 100,
-            category: "Other",
-            synced: false,
-            isWishListItem: false
-        });
-    }
+// exports.products = functions.https.onRequest(async (req, res) => {
+//     var path = req.path;
+//     if (path !== undefined && path == '/') {
+//         const products = await addFakeProducts();
+//         return res.status(200).send(products);
+//     } else {
+//         var index = path.substr(1);
+//         return res.status(200).send({
+//             key: `${index}`,
+//             name: `Item ${index}`,
+//             price: gen(),
+//             desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+//             image: `${index % 2 == 0 ? "http://static.techspot.com/images/products/2019/laptops/org/2019-03-22-product-6.jpg": "https://www.androidpolice.com/wp-content/uploads/2018/12/Samsung-Galaxy-S10-Plus-5K_2.png"}`,
+//             url: `https://us-central1-zenitech-solutions.cloudfunctions.net/products/${index}`,
+//             uploadTime: new Date().getTime(),
+//             quantity: 100,
+//             category: "Other",
+//             synced: false,
+//             isWishListItem: false
+//         });
+//     }
 
-});
+// });
 
-const addFakeProducts = () => {
-    var products = [];
-    for (let index = 0; index < 200; index++) {
-        const product = {
-            key: `${index}`,
-            name: `Item ${index}`,
-            price: gen(),
-            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            image: `${index % 2 == 0 ? "http://static.techspot.com/images/products/2019/laptops/org/2019-03-22-product-6.jpg": "https://www.androidpolice.com/wp-content/uploads/2018/12/Samsung-Galaxy-S10-Plus-5K_2.png"}`,
-            url: `https://us-central1-zenitech-solutions.cloudfunctions.net/products/${index}`,
-            uploadTime: new Date().getTime(),
-            quantity: 100,
-            category: "Other",
-            synced: false,
-            isWishListItem: false
-        };
-        products.push(product);
-    }
-    return products;
-};
+// const addFakeProducts = () => {
+//     var products = [];
+//     for (let index = 0; index < 200; index++) {
+//         const product = {
+//             key: `${index}`,
+//             name: `Item ${index}`,
+//             price: gen(),
+//             desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+//             image: `${index % 2 == 0 ? "http://static.techspot.com/images/products/2019/laptops/org/2019-03-22-product-6.jpg": "https://www.androidpolice.com/wp-content/uploads/2018/12/Samsung-Galaxy-S10-Plus-5K_2.png"}`,
+//             url: `https://us-central1-zenitech-solutions.cloudfunctions.net/products/${index}`,
+//             uploadTime: new Date().getTime(),
+//             quantity: 100,
+//             category: "Other",
+//             synced: false,
+//             isWishListItem: false
+//         };
+//         products.push(product);
+//     }
+//     return products;
+// };
